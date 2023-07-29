b3824edca016e9a41d7616d24448fe83708b28fe
electricwiz <thecountoftouscany@ciencias.unam.mx>
2023-07-29 14:33:50 +0000

    Savings class finished with the tweaking, also added a ROI calculator method with respect the initial deposit when creating the account. Keep also in mind I have made private the name variable, and also I have defined in the User interface the name as a private final variable with the respective .getName() method

--- src/Savings.java
+++ src/Savings.java
@@ -1,24 +1,50 @@
-public class Savings extends User implements IsProfitable, IsDepositable {
+public class Savings implements User, IsProfitable, IsDepositable {
+
+    static int id = 0;
+    private final String name;
+    public double total;
+    private final double initial;
 
     double interest;
 
     double profit;
 
     private Savings(String name, double deposit, double interest) {
-        super(name, deposit);
+        this.name = name;
+        this.initial = deposit;
+        this.total =  this.initial;
         this.interest = interest;
     }
 
     static User create(String name, double deposit, double interest) {
+        id++;
+        System.out.println("Creando cuenta de ahorro");
+
         return new Savings(name, deposit, interest);
     }
 
     public void profit(double interest, int ticks) {
-        this.total += this.total*interest*ticks;
+        this.profit += this.total*interest*ticks;
     }
 
     @Override
     public void deposit(double deposit) {
         this.total += deposit;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+    public String showTotal() {
+        return  "El total es: " + this.total;
+    }
+
+    public String showProfit() {
+        return "Las ganancias brutas han sido: " + profit;
+    }
+
+    public String showROI() {
+        return "Tu %ROI total es: " + this.profit/this.initial;
     }
 }
